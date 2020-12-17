9014cf3d9af7c7c66bb73a8cd5d1583ee4b3569b
Subeting <68261543+Subeting@users.noreply.github.com>
2020-12-17 22:42:43 +0800

    Add files via upload

--- go.sh
+++ go.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+iptables -t filter -N TORB
+ip6tables -t filter -N TORB
  curl https://raw.githubusercontent.com/Subeting/torban/main/tor.sh > /home/tor.sh
  chmod 755 /home/tor.sh
- ((echo "0 4 * * * bash /home/tor.sh";crontab -l)crontab)
+((echo "30 4 * * * bash /home/tor.sh" ; crontab -l)| crontab)
  wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubiBackup/doubi/master/ban_iptables.sh && chmod +x ban_iptables.sh && bash ban_iptables.sh banall
 
 curl https://raw.githubusercontent.com/Subeting/torban/main/tor.sh |bash
