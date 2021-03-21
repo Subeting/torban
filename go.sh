c33de97bf440d998bd9f6c65814adf092de2d9fa
Subeting <68261543+Subeting@users.noreply.github.com>
2021-03-22 06:57:00 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -4,7 +4,8 @@ ip6tables -t filter -N TORB
  curl https://raw.githubusercontent.com/Subeting/torban/main/tor.sh > /home/tor.sh
  echo "bash <(curl https://cdn.jsdelivr.net/gh/Subeting/doubi/ban_iptables.sh) banall" >>  /home/tor.sh
  chmod 755 /home/tor.sh
+ /home/tor.sh
 (echo "30 4 * * * bash /home/tor.sh" ; crontab -l)| crontab
  wget -N --no-check-certificate https://cdn.jsdelivr.net/gh/Subeting/doubi/ban_iptables.sh && chmod +x ban_iptables.sh && bash ban_iptables.sh banall
 
-/home/tor.sh
+
