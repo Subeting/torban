4438f7b3b998614b8db5dcbd556c1710d905ec92
Subeting <68261543+Subeting@users.noreply.github.com>
2021-07-12 20:33:17 +0800

    Update tor.sh
    V2

--- tor.sh
+++ tor.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
-v6=':'
-iptables -t filter -F TORB
-iptables -X TORB
-iptables -t filter -N TORB
-iptables -I INPUT -j TORB
-iptables -I OUTPUT -j TORB
+v6=":"
+echo "Begin to download target ips,process start in 5s"
+sleep 5s
 
-ip6tables -t filter -F TORB
-ip6tables -X TORB
-ip6tables -t filter -N TORB
-ip6tables -I INPUT -j TORB
-ip6tables -I OUTPUT -j TORB
+echo "Cleaning previous data"
+rulez="$(ip -4 route|grep metric|grep 23)
+$(ip -6 route|grep metric|grep 23)"
+IFS_old=$IFS
+IFS=$'\n'
+for arule in $rulez
+  do
+    IFS=$IFS_old
+    arr=($arule)
+    brule="$(echo ${arr[0]} ${arr[1]} ${arr[2]})"
+    ip route del $brule
+      
+  done
+IFS=$IFS_old
 
-curl https://www.dan.me.uk/torlist/ > torip.txt
-curl https://raw.githubusercontent.com/SecOps-Institute/Tor-IP-Addresses/master/tor-nodes.lst >> torip.txt
-curl https://raw.githubusercontent.com/Subeting/torban/main/bridge.txt >> torip.txt
+echo "Old version cleared.Downloading newer ones"
+uk="$(curl https://www.dan.me.uk/torlist/)"
+if [[ $uk =~ "too often" ]];
+then
+uk=""
+fi
+ips="$uk $(curl https://raw.githubusercontent.com/SecOps-Institute/Tor-IP-Addresses/master/tor-nodes.lst) $(curl https://raw.githubusercontent.com/Subeting/torban/main/bridge.txt)"
+ips="$(echo $ips|uniq)"
 echo "downloaded!Tor ban started."
- cat torip.txt | while read line
- do
-   if [[ $line == "" ]];
-   	then 
-   		continue
-   	fi
-   if [[ $line =~ $v6 ]];
+for line in $ips
+  do
+    if [[ $line == "" ]];
+    then 
+      continue
+    fi
+    if [[ $line =~ $v6 ]];
    then
-   ip6tables -t filter -A TORB -s ${line} -j DROP
-   ip6tables -t filter -A TORB -d ${line} -j DROP
+   ip -6 route add ${line}/64 dev lo metric 23
    else
-   iptables -t filter -A TORB -s ${line} -j DROP
-   iptables -t filter -A TORB -d ${line} -j DROP
+   line="$(echo $line|grep -v ":"|awk -F '.' '{print $1"."$2"."$3"."0}'|uniq|sort)"
+   ip route add ${line}/24 via 127.0.0.1 dev lo metric 23
    fi
- done
+  done
