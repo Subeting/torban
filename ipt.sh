64cf1f2deac690a41713f9fff7c115c9d1e6950f
Subeting <68261543+Subeting@users.noreply.github.com>
2021-07-13 23:29:51 +0800

    Update ipt.sh

--- ipt.sh
+++ ipt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "bash <(curl https://raw.githubusercontent.com/Subeting/doubi/master/ban_iptables.sh) banall" >  /home/tor.sh
+echo "bash <(curl https://raw.githubusercontent.com/Subeting/doubi/master/ban_iptables.sh) banspam" >  /home/tor.sh
 chmod 777 /home/tor.sh
 chmod +x /home/tor.sh
 (echo "*/5 * * * * bash /home/tor.sh" ; crontab -l)| crontab
-bash <(curl -Ls https://raw.githubusercontent.com/Subeting/doubi/master/ban_iptables.sh) banall
+bash <(curl -Ls https://raw.githubusercontent.com/Subeting/doubi/master/ban_iptables.sh) banspam
